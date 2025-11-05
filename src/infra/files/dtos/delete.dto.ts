0550c64b2ddd84232263eb994f8b4099afbc2174
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-11-05 16:57:30 -0300

    feat: add ApiProperty decorator to DeleteDto for file URL description and example

--- src/infra/files/dtos/delete.dto.ts
+++ src/infra/files/dtos/delete.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class DeleteDto {
+  @ApiProperty({
+    description: 'Public URL of the file to be deleted',
+    example: 'https://s3.example.com/mybucket/username/file-uuid.jpg',
+  })
   //@IsUrl() does not work since the url ends with the file extension and it bugs the validation
   @IsNotEmpty({ message: 'File URL is required.' })
   @IsString({ message: 'File URL must be a string.' })
