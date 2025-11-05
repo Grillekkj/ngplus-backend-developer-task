3f276dda95af2927cdb6951057c87c80b75301e3
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-11-05 13:53:44 -0300

    feat: implement UsersService with CRUD operations and validation

---
+++ src/modules/users/services/users.service.ts
@@ -0,0 +1,150 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { IReturnPaginated } from '../interfaces/returnPaginated.struct';
+import { IRequest } from 'src/modules/auth/interfaces/request.struct';
+import { AccountType } from '../enums/account-type.enum';
+import { IFindOne } from '../interfaces/findOne.struct';
+import { IFindAll } from '../interfaces/findAll.struct';
+import { ICreate } from '../interfaces/create.struct';
+import { IUpdate } from '../interfaces/update.struct';
+import { IRemove } from '../interfaces/remove.struct';
+import UsersEntity from '../entities/users.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private readonly usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  async create(data: ICreate): Promise<Partial<UsersEntity>> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(data.password, salt);
+
+    const newEntry = this.usersRepository.create({
+      ...data,
+      passwordHash: hashedPassword,
+    });
+    const savedNewEntry = await this.usersRepository.save(newEntry);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { passwordHash, refreshTokenHash, ...safeUser } = savedNewEntry;
+    return safeUser;
+  }
+
+  async findAll(data: IFindAll): Promise<IReturnPaginated> {
+    const [entries, total] = await this.usersRepository.findAndCount({
+      skip: (data.page - 1) * data.limit,
+      take: data.limit,
+      select: [
+        'id',
+        'profilePictureUrl',
+        'username',
+        'email',
+        'accountType',
+        'createdAt',
+        'updatedAt',
+        'ratingCount',
+        'lastLogin',
+      ],
+      order: { createdAt: 'DESC' },
+    });
+
+    if (entries.length === 0) {
+      throw new NotFoundException('No users found.');
+    }
+
+    return {
+      data: entries,
+      currentPage: data.page,
+      total,
+    };
+  }
+
+  async findOne(data: IFindOne, req?: IRequest): Promise<UsersEntity> {
+    const foundEntry = await this.usersRepository.findOne({
+      where: [
+        { id: data.id },
+        { username: data.username },
+        { email: data.email },
+      ],
+    });
+
+    if (!foundEntry) {
+      throw new NotFoundException('Entry not found.');
+    }
+
+    if (req) {
+      if (
+        req.user?.accountType !== AccountType.ADMIN &&
+        foundEntry.id !== req.user?.userId
+      ) {
+        throw new ForbiddenException('You can only access your own account.');
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { passwordHash, refreshTokenHash, ...safeUser } = foundEntry;
+      return safeUser as UsersEntity;
+    }
+
+    // Internal Calls (no req) will return everything to compare hashs
+    return foundEntry;
+  }
+
+  async update(data: IUpdate, req?: IRequest): Promise<Partial<UsersEntity>> {
+    const foundEntry = await this.usersRepository.findOne({
+      where: { id: data.id },
+    });
+
+    if (!foundEntry) {
+      throw new NotFoundException(`${data.id} not found.`);
+    }
+
+    if (req) {
+      if (
+        req.user?.accountType !== AccountType.ADMIN &&
+        foundEntry.id !== req.user?.userId
+      ) {
+        throw new ForbiddenException('You can only access your own account.');
+      }
+    }
+
+    const merged = this.usersRepository.merge(foundEntry, data);
+    const saved = await this.usersRepository.save(merged);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { passwordHash, refreshTokenHash, ...safeUser } = saved;
+    return safeUser;
+  }
+
+  async remove(data: IRemove, req?: IRequest): Promise<Partial<UsersEntity>> {
+    const foundEntry = await this.usersRepository.findOne({
+      where: { id: data.id },
+    });
+
+    if (!foundEntry) {
+      throw new NotFoundException(`${data.id} não encontrado.`);
+    }
+
+    if (req) {
+      if (
+        req.user?.accountType !== AccountType.ADMIN &&
+        foundEntry.id !== req.user?.userId
+      ) {
+        throw new ForbiddenException('You can only access your own account.');
+      }
+    }
+
+    await this.usersRepository.remove(foundEntry);
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { passwordHash, refreshTokenHash, ...safeUser } = foundEntry;
+    return safeUser;
+  }
+}
