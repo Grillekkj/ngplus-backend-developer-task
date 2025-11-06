4804aad8a5c4ea4595c02ed8f154ad47bca147a2
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-11-06 17:54:19 -0300

    feat: implement JwtPasswordResetStrategy for handling password reset JWT authentication

---
+++ src/modules/auth/strategies/jwt-password-reset.strategy.ts
@@ -0,0 +1,29 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtPasswordResetStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-password-reset',
+) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>(
+        'JWT_PASSWORD_RESET_SECRET',
+      ) as string,
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      userId: payload.userId,
+      email: payload.email,
+      jti: payload.jti,
+    };
+  }
+}
