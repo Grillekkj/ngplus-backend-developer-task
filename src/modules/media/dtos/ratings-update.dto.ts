30a2a627441c505e2de60347033fcd5db5647f1f
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-11-06 02:51:04 -0300

    fix: update validation message for maximum rating in UpdateRatingDto

--- src/modules/media/dtos/ratings-update.dto.ts
+++ src/modules/media/dtos/ratings-update.dto.ts
@@ -10,6 +10,6 @@ export class UpdateRatingDto {
   })
   @IsInt({ message: 'Rating must be an integer.' })
   @Min(1, { message: 'Rating must be at least 1.' })
-  @Max(5, { message: 'Rating must be at least 5.' })
+  @Max(5, { message: 'Rating must be at most 5.' })
   rating: number;
 }
