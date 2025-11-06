dacefc5b77707f3395941ee449e3744210e47f50
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-11-06 03:06:19 -0300

    refactor: clean up imports and formatting in MediaModule

--- src/modules/media/media.module.ts
+++ src/modules/media/media.module.ts
@@ -1,30 +1,16 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
+import { RatingsController } from './controllers/ratings.controller';
+import { MediaController } from './controllers/media.controller';
+import { RatingsService } from './services/ratings.service';
+import { MediaService } from './services/media.service';
 import { UsersModule } from '../users/users.module';
 import RatingEntity from './entities/rating.entity';
 import MediaEntity from './entities/media.entity';
-
-// Services
-import { MediaService } from './services/media.service';
-import { RatingsService } from './services/ratings.service';
-
-// Controllers
-import { MediaController } from './controllers/media.controller';
-import { RatingsController } from './controllers/ratings.controller';
-
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([MediaEntity, RatingEntity]),
-    UsersModule, // Importar UsersModule para ter acesso ao UsersService
-  ],
-  controllers: [
-    MediaController, // Controlador de Mídia (com permissões no service)
-    RatingsController, // Controlador de Ratings (com permissões no service)
-  ],
-  providers: [
-    MediaService, // Serviço de Mídia
-    RatingsService, // Serviço de Ratings
-  ],
+  imports: [TypeOrmModule.forFeature([MediaEntity, RatingEntity]), UsersModule],
+  controllers: [MediaController, RatingsController],
+  providers: [MediaService, RatingsService],
 })
 export class MediaModule {}
