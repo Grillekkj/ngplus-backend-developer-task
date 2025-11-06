12c5f01ac3fc7f60ed494873d295b4d06228909b
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-11-06 03:54:26 -0300

    refactor: remove UsersModule import from MediaModule

--- src/modules/media/media.module.ts
+++ src/modules/media/media.module.ts
@@ -5,11 +5,10 @@ import { RatingsController } from './controllers/ratings.controller';
 import { MediaController } from './controllers/media.controller';
 import { RatingsService } from './services/ratings.service';
 import { MediaService } from './services/media.service';
-import { UsersModule } from '../users/users.module';
 import RatingEntity from './entities/rating.entity';
 import MediaEntity from './entities/media.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([MediaEntity, RatingEntity]), UsersModule],
+  imports: [TypeOrmModule.forFeature([MediaEntity, RatingEntity])],
   controllers: [MediaController, RatingsController],
   providers: [MediaService, RatingsService],
 })
